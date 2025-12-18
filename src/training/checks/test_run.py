c317b9cc4b3b4ab801aa71acdac1883070f2c67f
ArikReuter <arik_reuter@gmx.de>
2025-12-18 17:26:52 +0000

    setup fivenode configs

--- src/training/checks/test_run.py
+++ src/training/checks/test_run.py
@@ -115,7 +115,7 @@ def run_simple_run_test(config_path):
 
 def main():
 
-    CONFIG_PATH = Path(__file__).parent.parent.parent.parent / "experiments" / "GraphConditioning" / "configs" / "five_nodes_lingaus_ultimate_gcn_and_soft_attention.yaml"
+    CONFIG_PATH = Path(__file__).parent.parent.parent.parent / "experiments" / "GraphConditioning" / "configs_5node" / "lingaus_ultimate_gcn_and_hard_attention.yaml"
     
     """Main test function."""
     print("=" * 60)
