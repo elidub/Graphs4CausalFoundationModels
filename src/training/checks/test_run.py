16e25ac54170c9360a2b15bc2170e1fa76da136d
ArikReuter <arik_reuter@gmx.de>
2026-01-05 12:13:39 +0100

    new configs where everything and nothing is hidden

--- src/training/checks/test_run.py
+++ src/training/checks/test_run.py
@@ -120,7 +120,7 @@ def run_simple_run_test(config_path):
 
 def main():
 
-    CONFIG_PATH = Path(__file__).parent.parent.parent.parent / "experiments" / "GraphConditioning" / "configs_5node_idk" / "lingaus_ancestor_5node_idk_gcn_and_softatt.yaml"
+    CONFIG_PATH = Path(__file__).parent.parent.parent.parent / "experiments" / "GraphConditioning" / "configs_5node_idk" / "lingaus_ancestor_5node_idk_gcn_and_softatt_hide_all.yaml"
     
     """Main test function."""
     print("=" * 60)
